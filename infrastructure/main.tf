e1356a2ca73de54f3650b8e4fe458361c44659fc
andes acosta <andesfelipeacostagarcia34@gmail.com>
2026-04-29 11:08:19 -0400

    change: bucket public access

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -219,6 +219,16 @@ resource "aws_s3_bucket_website_configuration" "hosting" {
   index_document { suffix = "index.html" }
 }
 
+resource "aws_s3_bucket_public_access_block" "website_public_access" {
+  bucket = var.website_bucket_name
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
 resource "aws_lambda_function" "presigned_url" {
   function_name    = var.presigned_url_lambda_name
   filename         = data.archive_file.presigned_url_lambda.output_path
