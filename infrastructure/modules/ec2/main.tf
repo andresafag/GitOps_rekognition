ff416cfec779a86d4b667ef0b5ec839ad50fe253
andes acosta <andesfelipeacostagarcia34@gmail.com>
2026-06-06 17:49:40 -0400

    test: github workflow actions

--- infrastructure/modules/ec2/main.tf
+++ infrastructure/modules/ec2/main.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "prometheus" {
   vpc_security_group_ids      = [aws_security_group.prometheus_sg.id]
   associate_public_ip_address = true
   iam_instance_profile        = var.iam_instance_profile != "" ? var.iam_instance_profile : null
-
+  key_name = var.key_name
   tags = merge({ Name = var.instance_name }, var.tags)
 
   user_data = <<-EOF
